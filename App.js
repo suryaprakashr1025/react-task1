a68940515c608da9863451ddbe85d23374142280
suryaprakashr1025 <110890454+suryaprakashr1025@users.noreply.github.com>
2022-09-29 20:47:40 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Topbar from './topbar';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+
+function App() {
+  return (
+    
+      <Topbar></Topbar>
+     
+  );
+}
+
+export default App;
